b3b992f5af28b36a62a12177137bad644907d09d
adrien-lebas <adrien@lebas.in>
2018-04-17 13:40:04 +0200

    I4: color extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand(100)
+background_color = "red"
+opacity = 0
 
 get '/' do
     guess = params["guess"].to_i
@@ -14,9 +16,17 @@ get '/' do
     elsif guess == secret_number
         message = "correct"
         secret_number_display = secret_number
+        opacity = 0
+        background_color = "green"
     end
     if (guess - secret_number).abs >=5
         message = "way " + message
+        opacity = 1
+    else opacity = 0.5
     end
-    erb :index, :locals => {:secret_number => secret_number_display, :message => message}
+    erb :index, :locals => {
+        :secret_number => secret_number_display,
+        :message => message,
+        :opacity => opacity,
+        :background_color => background_color}
 end
